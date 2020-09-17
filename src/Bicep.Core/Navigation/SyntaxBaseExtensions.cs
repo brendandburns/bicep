f2411ad976940474545da01967d0f23076d72156
Marcin Jastrzebski <majastrz@microsoft.com>
2020-09-16 19:49:02 -0700

    stuff

--- src/Bicep.Core/Navigation/SyntaxBaseExtensions.cs
+++ src/Bicep.Core/Navigation/SyntaxBaseExtensions.cs
@@ -1,15 +1,32 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 using System;
+using System.Collections.Generic;
+using Bicep.Core.Parser;
 using Bicep.Core.Syntax;
 
 namespace Bicep.Core.Navigation
 {
     public static class SyntaxBaseExtensions
     {
-        public static SyntaxBase? TryFindMostSpecificNode(this SyntaxBase root, int offset, Func<SyntaxBase, bool> predicate)
+        public static SyntaxBase? TryFindMostSpecificNodeInclusive(this SyntaxBase root, int offset, Func<SyntaxBase, bool> predicate) => 
+            TryFindMostSpecificNodeInternal(root, offset, predicate, inclusive: true);
+
+        public static SyntaxBase? TryFindMostSpecificNodeExclusive(this SyntaxBase root, int offset, Func<SyntaxBase, bool> predicate) => 
+            TryFindMostSpecificNodeInternal(root, offset, predicate, inclusive: false);
+
+        public static IList<Token> GetTokens(this SyntaxBase root)
         {
-            var visitor = new NavigationSearchVisitor(offset, predicate);
+            var tokens = new List<Token>();
+            var visitor = new TokenCollectorVisitor(tokens);
+            visitor.Visit(root);
+
+            return tokens;
+        }
+
+        private static SyntaxBase? TryFindMostSpecificNodeInternal(SyntaxBase root, int offset, Func<SyntaxBase, bool> predicate, bool inclusive)
+        {
+            var visitor = new NavigationSearchVisitor(offset, predicate, inclusive);
             visitor.Visit(root);
 
             return visitor.Result;
@@ -19,11 +36,13 @@ namespace Bicep.Core.Navigation
         {
             private readonly int offset;
             private readonly Func<SyntaxBase, bool> predicate;
+            private readonly bool inclusive;
 
-            public NavigationSearchVisitor(int offset, Func<SyntaxBase, bool> predicate)
+            public NavigationSearchVisitor(int offset, Func<SyntaxBase, bool> predicate, bool inclusive)
             {
                 this.offset = offset;
                 this.predicate = predicate;
+                this.inclusive = inclusive;
             }
 
             public SyntaxBase? Result { get; private set; }
@@ -31,7 +50,7 @@ namespace Bicep.Core.Navigation
             protected override void VisitInternal(SyntaxBase node)
             {
                 // check if offset is inside the node's span
-                if (node.Span.ContainsInclusive(offset))
+                if (CheckNodeContainsOffset(node))
                 {
                     // the node span contains the offset
                     // check the predicate
@@ -47,6 +66,26 @@ namespace Bicep.Core.Navigation
 
                 // the offset is outside of the node span
                 // there's no point to visit the children
+            }
+
+            private bool CheckNodeContainsOffset(SyntaxBase node) => this.inclusive
+                    ? node.Span.ContainsInclusive(offset)
+                    : node.Span.Contains(offset);
+        }
+
+        private sealed class TokenCollectorVisitor : SyntaxVisitor
+        {
+            private readonly IList<Token> tokens;
+
+            public TokenCollectorVisitor(IList<Token> tokens)
+            {
+                this.tokens = tokens;
+            }
+
+            protected override void VisitTokenInternal(Token token)
+            {
+                this.tokens.Add(token);
+                base.VisitTokenInternal(token);
             }
         }
     }
